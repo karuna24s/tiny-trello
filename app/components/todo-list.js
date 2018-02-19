f300596010587995b12f45540923efd17de2a649
karuna24s <karuna24s@gmail.com>
2018-02-19 11:03:06 -0500

    add a newItem action to todo-list component

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    addNewItem() {
+      this.get("list.items").push({ title: "New Item", description: "Item's Description" });
+    }
+  }
 });
