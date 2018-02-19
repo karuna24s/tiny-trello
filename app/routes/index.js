34ebc3fa8f71f9cbbec44339970f52d3d476ddde
karuna24s <karuna24s@gmail.com>
2018-02-19 10:59:49 -0500

    add a create new list and add new item link

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return [
-      { title: 'Default List',
+      { name: 'Default List',
         items: [{
           title: 'Item Title 1',
           description: 'Hire Karuna'
